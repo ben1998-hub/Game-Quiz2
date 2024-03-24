55ab72b99b17d0135d1aef8784f0e433e2d5fbc0
ben1998-hub johnson <ben.johnson1998@hotmail.co.uk>
2024-03-24 10:13:03 +0000

    Add question file and questions

---
+++ question.py
@@ -0,0 +1,45 @@
+questions = [
+    {
+        'question': 'How many valves does the hart have?',
+        'answer': ['14', '4', '5','6'],
+        'correct_answer': 'Red'
+    },
+    {
+        'question': 'What year was marmite invented?',
+        'answer': ['1955','2024','1897','1865'],
+        'correct_answer': '1897'
+    },
+    {
+        'question': 'What soprts drink was invented in Newcastle by a chemist?',
+        'answer': ['lucozade', 'coke', 'seven-up', 'pepsi'],
+        'correct_answer': 'lucozade'
+    },
+    {
+        'question': 'Jeff Bezos, CEO of Amazon started his business in what room?',
+        'answer': ['garage', 'kitchen','bed room','bath room'],
+        'correct_answer': 'garage'
+    },
+    {
+        'question': 'What is the biggest animal in the world?',
+        'answer': ['cat','dog','rat','blue whale'],
+        'correct_answer': 'blue whale'
+    },
+ 
+    {
+        'question': 'Which planet is closest to the sun?',
+        'answer': ['mercury', 'sun','earth','moon'],
+        'correct_answer': 'mercury'
+    },
+   
+    {
+        'question': 'Which actor played Del boy in only fools and hourses',
+        'answer': ['david jason', 'Bus Drivers', 'micheal jackson', 'john cena'],
+        'correct_answer': 'david jason'
+    },
+    {
+        'question': 'Which city had the first fashion week?',
+        'answer': ['new york', 'york', 'Ginny parris', 'liverpool'],
+        'correct_answer': 'new york'
+    }
+    
+]
