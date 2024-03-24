a3dce5a67fbbaea57710ad2468bc561e4fe841b7
ben1998-hub johnson <ben.johnson1998@hotmail.co.uk>
2024-03-24 10:07:57 +0000

    Add ci templa&add ruling to game

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from os import system, name  # import of os module
+import random  # for the use of randomising answers.
+from questions import questions # importing the questions from questions.py file for declutter.
+
+qq = questions
+finished = []
+
+
+# indexes the questions from the questions file.
+total = len(qq)
+indexes = ['A', 'B', 'C', 'D']
+score = 0 
+q_num = 0
+def clear():
+    """
+    function to clear 
+    """
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
